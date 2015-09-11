71166c9a27c730b44fa77b6394090d468242d74c
Toni Aho <toni.aho@siili.fi>
2015-09-12 02:06:10 +0300

    distance calc

--- src/js/components/timer.jsx
+++ src/js/components/timer.jsx
@@ -4,7 +4,26 @@ import Weather from './weather.jsx';
 
 export default class Timer extends React.Component {
 
-  construct(){
+  constructor() {
     super();
+    this.state ={elapsed: 0};
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.tick.bind(this), 100);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({elapsed: this.state.elapsed + 100})
+  }
+
+  render() {
+
+    return <div>{(this.state.elapsed / 1000).toFixed(1) } s</div>;
+
   }
 }
--- src/js/partials/track.jsx
+++ src/js/partials/track.jsx
@@ -10,24 +10,34 @@ export default class Track extends ReactComponent {
 
   constructor() {
     super();
-    this.state = {tracking: false, route: []};
+    this.state = {tracking: false, route: [], distance: 0.0};
   }
 
   render() {
-    var button = <button className="btn btn-block btn-lg btn-primary" onClick={this.handleClick.bind(this)}>Start tracking</button>;
+    var button = <button className="btn btn-block btn-lg btn-primary" onClick={this.handleClick.bind(this)}>Start
+      tracking</button>;
+    var panel = '';
     if (this.state.tracking) {
-      button = <button className="btn btn-block btn-lg btn-primary" onClick={this.handleClick.bind(this)}>Stop tracking</button>;
+      button = <button className="btn btn-block btn-lg btn-primary" onClick={this.handleClick.bind(this)}>Stop
+        tracking</button>;
+      panel = <div className="panel">
+        <div className="panel-body">
+          <div className="list-group-item-text">current: {this.state.distance} km</div>
+          <div className="list-group-item-text">tracking time: <Timer /></div>
+        </div>
+      </div>;
+
     }
 
-    var panel = <div className="panel"><div className="panel-body"><div className="list-group-item-text">current: 4,5 km</div><div className="list-group-item-text">tracking time</div> </div></div>;
     return <div className="text-center">
       <div className="page-header">
         <h1>Track your route</h1>
-        <GoogleMap marker={{latlng: dataModel.latlng, label: "Start"}} route={this.state.route}/>
-        {button}
-        {panel}
       </div>
+      {button}
+      <GoogleMap marker={{latlng: dataModel.latlng, label: "Start"}} route={this.state.route}/>
+      {panel}
     </div>
+
   }
 
   handleClick(event) {
@@ -36,7 +46,7 @@ export default class Track extends ReactComponent {
       navigator.geolocation.clearWatch(this.watcher);
     } else {
       this.setState({tracking: true});
-      this.watcher = navigator.geolocation.watchPosition(this.onNewPosition.bind(this),undefined, this.props.geOptions);
+      this.watcher = navigator.geolocation.watchPosition(this.onNewPosition.bind(this), undefined, this.props.geOptions);
 
     }
   }
@@ -46,7 +56,12 @@ export default class Track extends ReactComponent {
     var position = {lat: pos.coords.latitude, lng: pos.coords.longitude};
     var currentRoute = this.state.route;
     currentRoute.push(position);
-    this.setState({route: currentRoute});
+    var distance = this.calculateDistance(currentRoute);
+    this.setState({distance: distance, route: currentRoute});
+
+  }
+
+  calculateDistance(currentRoute) {
 
   }
 
